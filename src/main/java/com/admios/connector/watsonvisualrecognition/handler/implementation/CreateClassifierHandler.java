dddbd4c899404fbd9e8d87463e422dce9916fdff
AristidesMelendez <aristides.melendez@admios-sa.com>
2016-08-24 15:31:20 -0500

    Add options builder to Handler.

--- src/main/java/com/admios/connector/watsonvisualrecognition/handler/implementation/CreateClassifierHandler.java
+++ src/main/java/com/admios/connector/watsonvisualrecognition/handler/implementation/CreateClassifierHandler.java
@@ -1,5 +1,7 @@
 package com.admios.connector.watsonvisualrecognition.handler.implementation;
 
+import java.io.File;
+
 import com.admios.connector.watsonvisualrecognition.handler.CommonHandler;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.CreateClassifierOptions;
@@ -7,24 +9,31 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifi
 
 public class CreateClassifierHandler extends CommonHandler<VisualClassifier> {
 
-	private CreateClassifierOptions options;
-	
-	public CreateClassifierOptions getOptions() {
-		return options;
-	}
-
-	public void setOptions(CreateClassifierOptions options) {
-		this.options = options;
-	}
+	private CreateClassifierOptions.Builder builder = new CreateClassifierOptions.Builder();
 
 	public CreateClassifierHandler(VisualRecognition service) {
 		super(service);
 	}
-	
+
 	@Override
 	public VisualClassifier execute() {
-		return service.createClassifier(options).execute();
+		return service.createClassifier(builder.build()).execute();
 		
 	}
 
+	public CreateClassifierHandler addPositiveExamples(String classname, File file) {
+		//TODO Check if the file has content, that the reference already exist, not just is not null.
+		if(file != null){
+			builder.addClass("className", file);
+		}
+		return this;
+	}
+
+	public CreateClassifierHandler addNegativeExamples(File file) {
+		//TODO Check if the file has content, that the reference already exist, not just is not null.
+		if(file != null) {
+			builder.negativeExamples(file);
+		}
+		return this;
+	}
 }
