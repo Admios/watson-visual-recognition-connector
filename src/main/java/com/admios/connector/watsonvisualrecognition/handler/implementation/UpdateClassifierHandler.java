71a784138fc8c6e8ceac909df4501ea36928ce20
Enyert Vinas <enyert@Enyerts-MacBook-Pro.local>
2016-08-29 10:54:18 -0500

    Implementing UpdateClassifierHandler

---
+++ src/main/java/com/admios/connector/watsonvisualrecognition/handler/implementation/UpdateClassifierHandler.java
@@ -0,0 +1,25 @@
+package com.admios.connector.watsonvisualrecognition.handler.implementation;
+
+import com.admios.connector.watsonvisualrecognition.handler.CommonHandler;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.CreateClassifierOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
+
+public class UpdateClassifierHandler extends CommonHandler<VisualClassifier> {
+
+	private CreateClassifierOptions.Builder builder = new CreateClassifierOptions.Builder();
+	
+	public UpdateClassifierHandler(VisualRecognition service) {
+		super(service);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public VisualClassifier execute() {
+		// TODO Auto-generated method stub
+		//return service.createClassifier(builder.build()).execute();
+		return null;
+		
+	}
+
+}
