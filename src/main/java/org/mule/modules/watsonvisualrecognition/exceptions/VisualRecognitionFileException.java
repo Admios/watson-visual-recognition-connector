dbb40d6d6a26abb61a966c808bd1baea45d3410d
Gianluigi Pierini <gianluigi.pierini@Admioss-MacBook-Pro.local>
2017-02-08 10:34:47 -0500

    Updated the apidocs

--- src/main/java/org/mule/modules/watsonvisualrecognition/exceptions/VisualRecognitionFileException.java
+++ src/main/java/org/mule/modules/watsonvisualrecognition/exceptions/VisualRecognitionFileException.java
@@ -1,3 +1,6 @@
+/**
+ * (c) Copyright 2016 Admios. The software in this package is published under the terms of the Apache License Version 2.0, a copy of which has been included with this distribution in the LICENSE.md file.
+ */
 package org.mule.modules.watsonvisualrecognition.exceptions;
 
 public class VisualRecognitionFileException extends VisualRecognitionException {
